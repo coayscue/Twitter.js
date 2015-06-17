f1c96f8f223d5462e6ae4fdad477ac9df293050c
Christian Ayscue <coayscue@gmail.com>
2015-06-17 12:49:51 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require( 'express' );
+var app = express();
+var morgan = require('morgan');
+
+app.use(morgan("dev"));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get("/news", function(req, res){
+	res.send("news!");
+});
+
+var server = app.listen(3000, function () {
+
+ 	var host = server.address().address;
+ 	var port = server.address().port;
+
+ 	console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
