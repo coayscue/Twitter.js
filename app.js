c8fff147c3acdd2c70bbf6d9ab735d72bcc5594c
Christian Ayscue <coayscue@gmail.com>
2015-06-17 13:16:51 -0400

    Templates implemented

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require( 'express' );
 var app = express();
 var morgan = require('morgan');
+var swig = require("swig");
+
+swig.setDefaults({ cache: false });
 
 app.use(morgan("dev"));
 
+app.engine('html', swig.renderFile);
+app.set("view engine", "html");
+app.set("views", process.cwd()+"/views");
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.get("/news", function(req, res){
@@ -15,7 +23,7 @@ app.get("/news", function(req, res){
 var server = app.listen(3000, function () {
 
  	var host = server.address().address;
- 	var port = server.address().port;
+	var port = server.address().port;
 
  	console.log('Example app listening at http://%s:%s', host, port);
 
